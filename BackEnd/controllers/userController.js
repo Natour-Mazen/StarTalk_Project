85b7e84a070a8e3236fe1b79491a835bbd6a5ec7
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2023-12-06 19:52:47 +0100

    add more functions to prepare profile page

--- BackEnd/controllers/userController.js
+++ BackEnd/controllers/userController.js
@@ -130,6 +130,46 @@ class UserController {
         });
     }
 
+    // Retrieve all citations of a user
+    static async getAllUserCitations(req, res) {
+        try {
+            const user = await User.findOne({ discordId: req.client.id }).populate('allCitations');
+            if (user == null) {
+                return res.status(404).json({ message: 'Cannot find user' });
+            }
+            res.status(200).json(user.allCitations);
+        } catch (err) {
+            res.status(500).json({ message: err.message });
+        }
+    }
+
+    // Retrieve all favorites of a user
+    static async getAllUserFavorites(req, res) {
+        try {
+            const user = await User.findOne({ discordId: req.client.id }).populate('allFavorite');
+            if (user == null) {
+                return res.status(404).json({ message: 'Cannot find user' });
+            }
+            res.status(200).json(user.allFavorite);
+        } catch (err) {
+            res.status(500).json({ message: err.message });
+        }
+    }
+
+    // Retrieve all liked citations of a user
+    static async getAllUserLiked(req, res) {
+        try {
+            const user = await User.findOne({ discordId: req.client.id }).populate('allLiked');
+            if (user == null) {
+                return res.status(404).json({ message: 'Cannot find user' });
+            }
+            res.status(200).json(user.allLiked);
+        } catch (err) {
+            res.status(500).json({ message: err.message });
+        }
+    }
+
+
 
 
 }
