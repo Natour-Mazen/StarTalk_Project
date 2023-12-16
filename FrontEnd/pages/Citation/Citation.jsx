57d836c114163391dbbcffcb97d9f0ee1092eda1
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2023-12-16 17:49:38 +0100

    a citation prfoile pages is okay FrontEnd

--- FrontEnd/pages/Citation/Citation.jsx
+++ FrontEnd/pages/Citation/Citation.jsx
@@ -3,11 +3,18 @@ import { useParams } from 'react-router-dom';
 import { Panel } from 'primereact/panel';
 import axios from 'axios';
 import Base from "../../components/layout/Base";
+import {TabPanel, TabView} from "primereact/tabview";
+import {faAt, faClock, faMasksTheater} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {ScrollPanel} from "primereact/scrollpanel";
+import '../../assets/css/pages/Citation/Citation.css'
+import { Divider } from 'primereact/divider';
+
 
 export default function Citation({ match }) {
     const { id } = useParams();
     const [citation, setCitation] = useState(null);
-    const [humorcitatio, setHumor] = useState(null);
+    const [humorcitation, setHumor] = useState(null);
 
     const fetchData = async (url) => {
         try {
@@ -42,18 +49,58 @@ export default function Citation({ match }) {
 
     const formattedDate = formatDate(citation?.creationDate);
 
+
     return citation ? (
         <Base>
-            <div className="Citation">
-                <h1>Citation : {id}</h1>
-                <Panel header={citation.title}>
-                    <p><strong>Description:</strong> {citation.description}</p>
-                    <p><strong>Likes:</strong> {citation.numberLike}</p>
-                    <p><strong>Creation Date:</strong> {formattedDate}</p>
-                    <p><strong>Writer:</strong> {citation.writerName}</p>
-                    <p><strong>Humor:</strong> {humorcitatio?.name}</p>
-                </Panel>
-            </div>
+            <Panel header={<strong><p className="myPanelHeader">{citation.title}  </p></strong>} className="principalPanel">
+                <p>
+
+                       {citation.description}
+
+
+                </p>
+
+                <div className="infoCitaiton">
+                    <p><FontAwesomeIcon icon={faAt} title="writerName"/> {citation.writerName} </p>
+                    <p><FontAwesomeIcon icon={faMasksTheater} title="Humor"/> {humorcitation?.name} </p>
+                    <p><FontAwesomeIcon icon={faClock} title="creationDate"/> {formattedDate}</p>
+                </div>
+                <TabView>
+                    <TabPanel header={<><i className="pi pi-heart"/> Likes <small>( {citation.numberLike} )</small></>}
+                              style={{marginLeft: 'auto'}}>
+                        <ScrollPanel className='scrollPanelStyle'>
+                            {citation.likes.length > 0 ? (
+                                <ul className="ulStyleCitation">
+                                    {citation.likes.map((like, index) => (
+                                        <li key={index}><p><FontAwesomeIcon icon={faAt}/> {like.userName}</p></li>
+                                    ))}
+                                </ul>
+                            ) : (
+                                <p className='noneFavsLikePara'>
+                                    Looks like this citation is a hidden gem waiting to be discovered. More likes coming
+                                    soon ! 😊
+                                </p>
+                            )}
+                        </ScrollPanel>
+                    </TabPanel>
+                    <TabPanel header={<><i className="pi pi-star"/> Favs <small>( {citation.favs.length} )</small> </>}
+                              style={{marginRight: 'auto'}}>
+                        <ScrollPanel className='scrollPanelStyle'>
+                            {citation.favs.length > 0 ? (
+                                <ul className="ulStyleCitation">
+                                    {citation.favs.map((fav, index) => (
+                                        <li key={index}><p><FontAwesomeIcon icon={faAt}/> {fav.userName}</p></li>
+                                    ))}
+                                </ul>
+                            ) : (
+                                <p className='noneFavsLikePara'>
+                                    This citation is like a secret treasure. More favorites on the horizon ! 🌅
+                                </p>
+                            )}
+                        </ScrollPanel>
+                    </TabPanel>
+                </TabView>
+            </Panel>
         </Base>
     ) : (
         <Base>
