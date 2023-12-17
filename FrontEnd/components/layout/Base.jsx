738b64008342c9d6b728d104ffabe0eaf7e426a4
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2023-12-17 15:40:54 +0100

    don't show the rightSideBar when we are on the userpanel admin FRONT

--- FrontEnd/components/layout/Base.jsx
+++ FrontEnd/components/layout/Base.jsx
@@ -8,10 +8,14 @@ import '../../assets/css/components/layout/Base.css';
 import React, {useContext, useEffect} from "react";
 import {UserContext} from "../../utils/UserAuthContext";
 import ScrollToTopButton from "../Button/ScrollToTopButton";
+import {useLocation} from "react-router-dom";
 
 export default function Base({ children })
 {
-    const { isAuthenticated, role, fetchUserAfterLoginData} = useContext(UserContext);
+    const location = useLocation();
+    //We don't show the right part when we are in the admin users panel
+    const isSpecificRoute = location.pathname === '/admin/userspanel';
+
     return (
         <div className="Base">
             <Menu/>
@@ -20,8 +24,15 @@ export default function Base({ children })
                 {children}
             </Main>
             <ScrollToTopButton />
-            <Divider layout="vertical" className="separator"/>
-            <RightSideBar/>
+            {!isSpecificRoute ?  (
+                <>
+                    <Divider layout="vertical" className="separator"/>
+                    <RightSideBar/>
+                </>
+            ) : (
+                <></>
+                )
+            }
         </div>
     );
 }
