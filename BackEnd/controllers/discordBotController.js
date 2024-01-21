6fc3aa728043177c67c21a10bb54170b08be5b99
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2024-01-21 23:13:51 +0100

    factorize code DISCORDBOT

--- BackEnd/controllers/discordBotController.js
+++ BackEnd/controllers/discordBotController.js
@@ -1,18 +1,18 @@
 const User = require('../models/User');
 const Citation = require('../models/Citation');
 class DiscordBotController {
-    // Helper function to retrieve a random number of citations for a specific user
-    static async getRandomCitationsForUser(discordId) {
+// Helper function to retrieve a random number of citations for a specific user
+    static async getRandomCitationsForUser(discordId, citationType = 'allCitations') {
         // Find the user by their discordId
-        const user = await User.findOne({ discordId: discordId }).populate('allCitations');
+        const user = await User.findOne({ discordId: discordId }).populate(citationType);
 
         // If the user is not found
         if (user == null) {
             throw new Error("Cannot find user")
         }
 
-        // Get all citations written by the user
-        const userCitations = user.allCitations;
+        // Get all citations of the user
+        const userCitations = user[citationType];
 
         // Generate a random number between 1 and 4
         const randomNumber = Math.floor(Math.random() * 4) + 1;
@@ -42,59 +42,14 @@ class DiscordBotController {
 
     // Helper function to retrieve a random number of favorite citations for a specific user
     static async getRandomFavoritesForUser(client) {
-        // Find the user by their discordId
-        const user = await User.findOne({ discordId: client.id }).populate('allFavorite');;
-
-        // If the user is not found, return a 404 response
-        if (user == null) {
-            throw new Error("Cannot find user")
-        }
-        // Get all favorite citations of the user
-        const userFavorites = user.allFavorite;
-
-        // Generate a random number between 1 and 4
-        const randomNumber = Math.floor(Math.random() * 4) + 1;
-
-        // Get a random subset of citations
-        const randomCitations = userFavorites.sort(() => 0.5 - Math.random()).slice(0, randomNumber);
-
-        // Map the random citations to an array of objects with only title and description
-        const citationsToSend = randomCitations.map(citation => ({
-            title: citation.title,
-            description: citation.description
-        }));
-        // Send the random favorite citations
-        return citationsToSend
+        return this.getRandomCitationsForUser(client.id, 'allFavorite');
     }
-
 
     // Helper function to retrieve a random number of likes citations for a specific user
     static async getRandomLikesForUser(client) {
-        // Find the user by their discordId
-        const user = await User.findOne({ discordId: client.id }).populate('allLiked');;
-
-        // If the user is not found, return a 404 response
-        if (user == null) {
-            throw new Error("Cannot find user")
-        }
-        // Get all favorite citations of the user
-        const userLikes = user.allLiked;
-
-        // Generate a random number between 1 and 4
-        const randomNumber = Math.floor(Math.random() * 4) + 1;
-
-        // Get a random subset of citations
-        const randomCitations = userLikes.sort(() => 0.5 - Math.random()).slice(0, randomNumber);
-
-        // Map the random citations to an array of objects with only title and description
-        const citationsToSend = randomCitations.map(citation => ({
-            title: citation.title,
-            description: citation.description
-        }));
-        // Send the random favorite citations
-        return citationsToSend
+        return this.getRandomCitationsForUser(client.id, 'allLiked');
     }
-
+    
     // Add a created citation to allCitations
     static async addCitation(client,title,desc) {
 
